83e51f710f1929f7cd2ad1c846c5ca8c8d035ce9
omega1100100 <omega1100100@gmail.com>
2018-09-30 03:33:03 +0700

    Dũng done header

--- src/components/Anchor1.jsx
+++ src/components/Anchor1.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './Anchor1.css';
 import light_bulb from '../images/bongden.png';
+import Header from './Header';
 
 const DEADLINE = '10/19/2018';
 
@@ -40,37 +41,40 @@ class App extends Component {
 
   render() {
     return (
-      <div className="fa_background">
-        <div className="fa">
-          <div className="fa_big_hulb_section">
-            <img src={light_bulb} alt="light_bulb" />
-          </div>
-          <div className="fa_content_section row">
-            <h2 className="fa_vsl">Vòng sơ loại</h2>
-            <h1 className="fa_nyt">Nộp ý tưởng</h1>
-            <p className="fa_lorem">
-              Dưới con mắt của chúng tôi, mỗi lập trình viên là một tài năng. Họ đều có điểm chung là xuất phát từ đam mê, đi lên bằng khổ luyện, và
-              tỏa sáng bằng tài năng và trí tuệ tuyệt vời. Và chúng tôi tin rằng, '<span>Coding Project 2018</span>' chính là sân chơi đáng giá dành
-              cho những coder trẻ luôn khao khát được tỏa sáng, được cháy hết mình với đam mê.
-            </p>
-            <h1 className="fa_number">
-              <span>{this.leading0(this.state.days)}</span>
-              &nbsp;:&nbsp;
-              {this.leading0(this.state.hours)}
-              &nbsp;:&nbsp;
-              {this.leading0(this.state.minutes)}
-              &nbsp;:&nbsp;
-              {this.leading0(this.state.seconds)}
-            </h1>
-            <h3 className="fa_ngaygio">
-              Ngày&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              Giờ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              Phút&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Giây
-            </h3>
-            <input type="button" className="fa_btn" value="ĐĂNG KÝ NGAY" />
+      <aside>
+        <Header showBtn={false} />
+        <div className="fa_background">
+          <div className="fa">
+            <div className="fa_big_hulb_section">
+              <img src={light_bulb} alt="light_bulb" />
+            </div>
+            <div className="fa_content_section row">
+              <h2 className="fa_vsl">Vòng sơ loại</h2>
+              <h1 className="fa_nyt">Nộp ý tưởng</h1>
+              <p className="fa_lorem">
+                Dưới con mắt của chúng tôi, mỗi lập trình viên là một tài năng. Họ đều có điểm chung là xuất phát từ đam mê, đi lên bằng khổ luyện, và
+                tỏa sáng bằng tài năng và trí tuệ tuyệt vời. Và chúng tôi tin rằng, '<span>Coding Project 2018</span>' chính là sân chơi đáng giá dành
+                cho những coder trẻ luôn khao khát được tỏa sáng, được cháy hết mình với đam mê.
+              </p>
+              <h1 className="fa_number">
+                <span>{this.leading0(this.state.days)}</span>
+                &nbsp;:&nbsp;
+                {this.leading0(this.state.hours)}
+                &nbsp;:&nbsp;
+                {this.leading0(this.state.minutes)}
+                &nbsp;:&nbsp;
+                {this.leading0(this.state.seconds)}
+              </h1>
+              <h3 className="fa_ngaygio">
+                Ngày&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Giờ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Phút&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Giây
+              </h3>
+              <input type="button" className="fa_btn" value="ĐĂNG KÝ NGAY" />
+            </div>
           </div>
         </div>
-      </div>
+      </aside>
     );
   }
 }
