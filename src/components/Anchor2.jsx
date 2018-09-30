563543f9af7550243a5eb435e095ecfb84be54d1
omega1100100 <omega1100100@gmail.com>
2018-09-30 14:52:09 +0700

    Sửa một số chỗ ở anchor1, sửa timeline anchor 4

--- src/components/Anchor2.jsx
+++ src/components/Anchor2.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import Header from './Header';
-
 import ci_intro from '../videos/ci-intro.mp4';
 
 class Anchor2 extends React.Component {
   render() {
     return (
-      <aside>
-        <Header showBtn />
-        <video width="100%" controls>
-          <source src={ci_intro} type="video/mp4" />
-        </video>
-      </aside>
+      <video width="100%" height="auto" controls autoPlay>
+        <source src={ci_intro} type="video/mp4" />
+      </video>
     );
   }
 }
