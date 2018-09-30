5f233ea26ce68adc3e067778e7d79fd600baf49d
omega1100100 <omega1100100@gmail.com>
2018-09-30 17:33:16 +0700

    update anchor 6

--- src/components/Anchor7.js
+++ src/components/Anchor7.js
@@ -7,35 +7,45 @@ import FPT from '../images/Screen7/fpt.png';
 import Sota from '../images/Screen7/sota.png';
 
 export default class Sponsor extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="sp-content">
-                    <div className="sp-div-title">
-                        <span>đối tác</span>
-                    </div>
-                    <br/>
-                    <div className="sp-silver">
-                        <span>Nhà tài trợ bạc</span>
-                        <br/>
-                        <a href="https://vietis.com.vn/" className="fix-margin-a" target="_blank"><img src={VietIS} alt="VietIS" width="20%"/></a>
-                        <a href="https://techkids.vn/" target="_blank"><img src={TechKids} alt="TechKid" width="30%"/></a>
-                        <a href="https://www.topcv.vn/" target="_blank"><img src={TopCV} alt="TopCV" width="18%"  className="fix-topcv"/></a>
-                    </div>
-                    
-                    <div className="sp-bronze">
-                        <span>Đồng tài trợ</span>
-                        <br/>
-                        <a href="https://www.fpt-software.com/" className="fix-margin-a" target="_blank"><img src={FPT} alt="FPT Software" width="20%"/></a>
-                        <a href="https://sotatek.com/" target="_blank"><img src={Sota} alt="Sota Tek" width="27%"/></a>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="sp-content">
+          <div className="sp-div-title">
+            <span>đối tác</span>
+          </div>
+          <br />
+          <div className="sp-silver">
+            <span>Nhà tài trợ bạc</span>
+            <br />
+            <a href="https://vietis.com.vn/" className="fix-margin-a" target="_blank" rel="noopener noreferrer">
+              <img src={VietIS} alt="VietIS" width="20%" />
+            </a>
+            <a href="https://techkids.vn/" target="_blank" rel="noopener noreferrer">
+              <img src={TechKids} alt="TechKid" width="30%" />
+            </a>
+            <a href="https://www.topcv.vn/" target="_blank" rel="noopener noreferrer">
+              <img src={TopCV} alt="TopCV" width="18%" className="fix-topcv" />
+            </a>
+          </div>
+
+          <div className="sp-bronze">
+            <span>Đồng tài trợ</span>
+            <br />
+            <a href="https://www.fpt-software.com/" className="fix-margin-a" target="_blank" rel="noopener noreferrer">
+              <img src={FPT} alt="FPT Software" width="20%" />
+            </a>
+            <a href="https://sotatek.com/" target="_blank" rel="noopener noreferrer">
+              <img src={Sota} alt="Sota Tek" width="27%" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,23 @@ import Anchor1 from './Anchor1';
 import Anchor2 from './Anchor2';
 import Anchor3 from './Anchor3';
 import Anchor4 from './Anchor4';
+import Anchor5 from './Anchor5';
+import Anchor6 from './Anchor6';
+import Anchor7 from './Anchor7';
 import Header from './Header';
 export default class Home extends Component {
-    render(){
-        return(
-            <div>
-            <Header/>
-            <Anchor1/>
-            <Anchor2/>
-            <Anchor3/>
-            <Anchor4/>
-            </div>
-        )
-    }
-};
+  render() {
+    return (
+      <div>
+        <Header />
+        <Anchor1 />
+        <Anchor2 />
+        <Anchor3 />
+        <Anchor4 />
+        <Anchor5 />
+        <Anchor6 />
+        <Anchor7 />
+      </div>
+    );
+  }
+}
